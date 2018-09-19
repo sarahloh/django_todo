4ce810c0628fbac20deb760567f0e531b14f2adb
Sarah Loh <sarah.loh0708@gmail.com>
2018-09-19 21:24:12 +0100

    Prep requirements and settings for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse("postgres://smckrdslygbsqf:98679624d23c94df6ba470034580b95cd6a8cdfe5dfe9642201308934dde50bc@ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/dack9sqlmrug7g")
 }
 
 
